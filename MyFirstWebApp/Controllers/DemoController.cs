cf53bde9966e1417f622200a7a84ddda308872ae
Terry0117 <jsps000106@gmail.com>
2024-05-28 16:41:37 +0800

    加入專案檔案。

---
+++ MyFirstWebApp/Controllers/DemoController.cs
@@ -0,0 +1,48 @@
+﻿using MyFirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewData["Name"] = "陳小強";
+            ViewBag.Name = "陳小強";
+            TempData["Name"] = "陳小強";
+            return RedirectToAction("ShowTempData");
+        }
+
+        public ActionResult ShowTempData()
+        {
+            return View();
+        }
+
+        public ActionResult ShowMembers()
+        {
+            var memberA = new Member() { Id = 1, Name = "Alex", Age = 12 };
+            var memberB = new Member() { Id = 2, Name = "Ben", Age = 34 };
+            var memberC = new Member() { Id = 3, Name = "Carol", Age = 56 };
+            var memberList = new List<Member>() { memberA, memberB, memberC};
+            ViewData["Members"] = memberList;
+            ViewBag.Members = memberList;
+            return View();
+        }
+
+        // GET: Demo/ShowHelloWorld
+        public string ShowHelloWorld()
+        {
+            return "Hello World!" ;
+        }
+
+        public string ShowPrice(string product, int price)
+        {
+            return $"商品是:{product}，價格是{price}";
+        }
+    }
+}
