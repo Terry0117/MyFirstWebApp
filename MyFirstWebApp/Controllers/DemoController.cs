99abc71e799b9234adc3f5eb59a20b787834f4c6
Terry0117 <jsps000106@gmail.com>
2024-06-04 15:49:07 +0800

    Controller View 傳遞資料練習

--- MyFirstWebApp/Controllers/DemoController.cs
+++ MyFirstWebApp/Controllers/DemoController.cs
@@ -28,21 +28,36 @@ namespace MyFirstWebApp.Controllers
             var memberA = new Member() { Id = 1, Name = "Alex", Age = 12 };
             var memberB = new Member() { Id = 2, Name = "Ben", Age = 34 };
             var memberC = new Member() { Id = 3, Name = "Carol", Age = 56 };
-            var memberList = new List<Member>() { memberA, memberB, memberC};
+            var memberList = new List<Member>() { memberA, memberB, memberC };
             ViewData["Members"] = memberList;
             ViewBag.Members = memberList;
-            return View();
+            return View(memberList);
         }
 
         // GET: Demo/ShowHelloWorld
         public string ShowHelloWorld()
         {
-            return "Hello World!" ;
+            return "Hello World!";
         }
 
         public string ShowPrice(string product, int price)
         {
             return $"商品是:{product}，價格是{price}";
+        }
+
+        public ActionResult ShowShoppingList()
+        {
+            var member = new Member() { Id = 1, Name = "Alex", Age = 12 };
+            var products = new List<Product>();
+            products.Add(new Product { Number = 1, Name = "Apple", Price = 30 });
+            products.Add(new Product { Number = 2, Name = "Banana", Price = 50 });
+            products.Add(new Product { Number = 3, Name = "Cherry", Price = 30 });
+
+            var memberInfo = new MemberInfoViewModel();
+            memberInfo.Member = member;
+            memberInfo.Products = products;
+
+            return View(memberInfo);
         }
     }
 }
---
+++ MyFirstWebApp/Controllers/LogInController.cs
@@ -0,0 +1,45 @@
+﻿using MyFirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // 使用FormCollection取得Form回傳的資料
+        //[HttpPost]
+        //public ActionResult Verify(FormCollection obj)
+        //{
+        //    ViewBag.Account = obj["account"];
+        //    ViewBag.Password = obj["password"];
+
+        //    return View();
+        //}
+        //直接將form回傳的元素寫在參數內
+        //[HttpPost]
+        //public ActionResult Verify(string account, string password)
+        //{
+        //    ViewBag.Account = account;
+        //    ViewBag.Password = password;
+
+        //    return View();
+        //}
+        //自行建立類別與屬性，來對應form裡面元素name屬性
+        [HttpPost]
+        public ActionResult Verify(LogInViewModel vm)
+        {
+            ViewBag.Account = vm.Account;
+            ViewBag.Password = vm.Password;
+
+            return View();
+        }
+    }
+}
